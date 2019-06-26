2cb960506fe9f80f204c374564b1d7daf21b0453
anShuang <1205697836@qq.com>
2019-06-26 10:47:00 +0800

    demo1-vue change

---
+++ src/components/demo1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <div>列表渲染</div>
+      <div>{{msg}}</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        msg : "anshuang"
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
