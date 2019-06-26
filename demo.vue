d54495ab5a82d270f5db3da023b0a565d59622eb
anShuang <1205697836@qq.com>
2019-06-26 12:09:24 +0800

    first change commit

---
+++ demo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+      <div>vue cli</div>
+      <ul>
+        <li v-for="(item,index) in list" v-bind:key="index" @click="choose(index)"
+         :class="{'active':index == current && current !== ''}">
+            {{item}}
+        </li>
+      </ul>
+      <button @click="add()">添加</button>
+      <ul>
+        <li v-for="(item,index) in targetlist" :key="index">
+          {{item}}
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo',
+    data(){
+      return{
+        list: [1,2,3,4,5,6,7,8,9],
+        isactive: true,
+        current: '',
+        targetlist: []
+      }
+    },
+    methods: {
+      choose(i){
+        this.current = i;
+
+      },
+      add(){
+        if(this.current === ''){
+          return;
+        }
+        this.targetlist.push(this.list[this.current]);
+        this.current = '';
+      }
+    }
+  };
+</script>
+
+<style scoped>
+li.active{
+  background-color: green;
+}
+</style>
---
+++ src/views/hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>hello</div>
+    <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        msg : "anshuang"
+      }
+    }
+  }
+</script>
